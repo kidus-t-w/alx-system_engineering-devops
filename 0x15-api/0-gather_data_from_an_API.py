0a4a897dab8770adcfb0f0760ec7173eca7a7df2
kidus-t-w <kidusk69@gmail.com>
2024-05-02 11:51:54 +0300

    fixed task

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Returns information about user TODO progress"""
+
+import requests
+import sys
+
+
+arg1 = int(sys.argv[1])
+user = requests.get('https://jsonplaceholder.typicode.com/users/{}' .format(arg1)).json()
+response = requests.get('https://jsonplaceholder.typicode.com/todos/')
+API_data = response.json()
+
+employee = user["name"]
+completed = []
+completed_task = 0
+tasks = 0
+for i in API_data:
+    if (i["userId"] == arg1):
+        tasks += 1
+        if (i["completed"] == True):
+            completed_task += 1
+            completed.append(i["title"])
+print("Employee {} is done with tasks({}/{}):".format(employee, completed_task, tasks))
+for i in completed:
+    print(" "*4 + i)
